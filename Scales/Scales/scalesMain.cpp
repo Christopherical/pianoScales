71c092c5d83eb2801fe03fbbba2df49556cce7d0
Christopherical <chris_dixon1989@hotmail.com>
2020-10-20 18:22:37 +0100

    added a close when reading file / added vector / read through vector

--- Scales/Scales/scalesMain.cpp
+++ Scales/Scales/scalesMain.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
+	ifstream scalesFile("theScales.txt");	
+	vector<string> scales;
+	string item;
 
-	ifstream scalesFile("theScales.txt");
-	string scale;	
-
-	while (scalesFile >> scale) {
-		cout << scale;
+	if (scalesFile.is_open()) {
+		while (getline(scalesFile, item)) {			
+			scales.push_back(item);
+		}
+		scalesFile.close();
 	}
+
+	for (int i=0; i<scales.size(); i++) {
+		cout << scales[i] << endl;
+	}	
 
 	return 0;
 }
