34eb3e5ea45d2331dca4ccfd809865b04e1eebe2
Christopherical <chris_dixon1989@hotmail.com>
2020-10-21 19:51:25 +0100

    Added rand

--- Scales/Scales/scalesMain.cpp
+++ Scales/Scales/scalesMain.cpp
@@ -2,24 +2,33 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
 int main(){
-	ifstream scalesFile("theScales.txt");	
+	ifstream scalesFile("theScales.txt");
+	vector<string> dynamics = { "p", "mf", "f" };
+	vector<string> articulation = { "legato", "stacatto" };
 	vector<string> scales;
 	string item;
 
+	srand(time(0));
+
+	
 	if (scalesFile.is_open()) {
 		while (getline(scalesFile, item)) {			
 			scales.push_back(item);
 		}
 		scalesFile.close();
 	}
+	
 
-	for (int i=0; i<scales.size(); i++) {
-		cout << scales[i] << endl;
+	for (int i = 0; i < 100; i++) {
+		cout << (rand() % scales.size()) << endl;
 	}
+	
+	
 	string a;
 	cin >> a;
 	return 0;
